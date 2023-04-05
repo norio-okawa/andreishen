764b4156260e2ac16429822e9482bfe737c5d9ba
norio-okawa <norio.okawa.1123@gmail.com>
2023-04-05 11:02:28 -0500

    first commit

---
+++ src/constants.js
@@ -0,0 +1,510 @@
+export const techStack = [
+  {
+    name: "HTML",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+  },
+  {
+    name: "CSS",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg",
+  },
+  // { name: "Sass", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" },
+  {
+    name: "Bootstrap",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg",
+  },
+  {
+    name: "Tailwind CSS",
+    link: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+  },
+  {
+    name: "Material UI",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
+  },
+
+  {
+    name: "Javascript",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+  },
+  {
+    name: "TypeScript",
+    link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+  },
+  {
+    name: "React",
+    link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+  },
+  {
+    name: "Next",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+  },
+  {
+    name: "Redux",
+    link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg",
+  },
+
+  {
+    name: "React Native",
+    link: "https://pagepro.co/blog/wp-content/uploads/2020/03/react-native-logo-884x1024.png",
+  },
+  {
+    name: "Angular",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg",
+  },
+  {
+    name: "Vue",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg",
+  },
+  {
+    name: "Vuetify",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuetify/vuetify-original.svg",
+  },
+  {
+    name: "jQuery",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original.svg",
+  },
+
+  {
+    name: "NodeJS",
+    link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+  },
+  {
+    name: "Express",
+    link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
+  },
+  {
+    name: "PHP",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg",
+  },
+  {
+    name: "Laravel",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-plain.svg",
+  },
+  {
+    name: "Wordpress",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-original.svg",
+  },
+
+  {
+    name: "CodeIgniter",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/codeigniter/codeigniter-plain.svg",
+  },
+  {
+    name: "MySQL",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain.svg",
+  },
+  {
+    name: "MS SQL",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/microsoftsqlserver/microsoftsqlserver-plain.svg",
+  },
+  {
+    name: "Oracle",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/oracle/oracle-original.svg",
+  },
+  {
+    name: "Mongo DB",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg",
+  },
+
+  {
+    name: "PostgreSQL",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+  },
+  {
+    name: "sqlite",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original.svg",
+  },
+  {
+    name: "GraphGL",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain.svg",
+  },
+  {
+    name: "Solidity",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/solidity/solidity-original.svg",
+  },
+  {
+    name: "Rust",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rust/rust-plain.svg",
+  },
+
+  {
+    name: "AWS",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original.svg",
+  },
+  {
+    name: "kubernetes",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain.svg",
+  },
+  {
+    name: "Docker",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain.svg",
+  },
+  {
+    name: "Firebase",
+    link: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+  },
+  {
+    name: "VS Code",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
+  },
+
+  {
+    name: "C",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-plain.svg",
+  },
+  {
+    name: "C++",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg",
+  },
+  {
+    name: "C#",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg",
+  },
+  {
+    name: ".net",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dot-net/dot-net-original.svg",
+  },
+  {
+    name: "Azure",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-plain.svg",
+  },
+
+  {
+    name: "Git",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg",
+  },
+  {
+    name: "Heroku",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain.svg",
+  },
+  {
+    name: "Postman",
+    link: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+  },
+  {
+    name: "Bash",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg",
+  },
+  {
+    name: "Vim",
+    link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vim/vim-original.svg",
+  },
+
+  // {name: "", link: "https://repository-images.githubusercontent.com/59065830/b62be480-45d2-11ea-9989-803db0f9c44d"}
+];
+export const histories = [
+  {
+    logo: "robotalker_logo.svg",
+    title: "Senior full stack developer",
+    company: "Robotalker",
+    desc: "As a full stack developer, to contribute to development of the software that manages the Robotalker, my main role was to develop new features for the new version.",
+    period: "Jan 2022 – Jan 2023",
+    skills: [
+      {
+        name: "Angular",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg",
+      },
+      {
+        name: "Javascript",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+      },
+      {
+        name: "TypeScript",
+        link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+      },
+      {
+        name: "React",
+        link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+      },
+      {
+        name: "Vue",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg",
+      },
+      {
+        name: "Vuetify",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuetify/vuetify-original.svg",
+      },
+      {
+        name: "jQuery",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original.svg",
+      },
+
+      {
+        name: "NodeJS",
+        link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+      },
+      {
+        name: "Express",
+        link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
+      },
+      {
+        name: "C",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-plain.svg",
+      },
+      {
+        name: "C++",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg",
+      },
+      {
+        name: "C#",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg",
+      },
+      {
+        name: ".net",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dot-net/dot-net-original.svg",
+      },
+      {
+        name: "Azure",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-plain.svg",
+      },
+      {
+        name: "AWS",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original.svg",
+      },
+      {
+        name: "kubernetes",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain.svg",
+      },
+      {
+        name: "Docker",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain.svg",
+      },
+      {
+        name: "Firebase",
+        link: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+
+      {
+        name: "Git",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg",
+      },
+      {
+        name: "Postman",
+        link: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+      },
+      {
+        name: "Bash",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg",
+      },
+      {
+        name: "Vim",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vim/vim-original.svg",
+      },
+    ],
+  },
+  {
+    logo: "onetake-ai.jpg",
+    company: "Onetake AI",
+    title: "Senior full stack developer",
+    desc: "Developed modern online video player with HTML5/Canvas by using React.JS/Fabric.JS from scratch thriving in fast-paced, iterative startup environment.",
+    period: "Jan 2020 – Jan 2022",
+    skills: [
+      {
+        name: "React",
+        link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+      },
+      {
+        name: "Next",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+      },
+      {
+        name: "Redux",
+        link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg",
+      },
+
+      {
+        name: "React Native",
+        link: "https://pagepro.co/blog/wp-content/uploads/2020/03/react-native-logo-884x1024.png",
+      },
+      {
+        name: "Angular",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg",
+      },
+      {
+        name: "Vue",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg",
+      },
+      {
+        name: "Vuetify",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuetify/vuetify-original.svg",
+      },
+      {
+        name: "jQuery",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original.svg",
+      },
+      {
+        name: "Bootstrap",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg",
+      },
+      {
+        name: "Tailwind CSS",
+        link: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+      },
+      {
+        name: "Material UI",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
+      },
+
+      {
+        name: "NodeJS",
+        link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+      },
+      {
+        name: "Express",
+        link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
+      },
+      {
+        name: "PHP",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg",
+      },
+      {
+        name: "Laravel",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-plain.svg",
+      },
+      {
+        name: "GraphGL",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain.svg",
+      },
+      {
+        name: "Solidity",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/solidity/solidity-original.svg",
+      },
+      {
+        name: "Rust",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rust/rust-plain.svg",
+      },
+    ],
+  },
+  {
+    logo: "kamchihau.jpg",
+    company: "Kamchihau",
+    title: "Web developer",
+    desc: "Used Angular & React & Vue & jQuery to make Reactive Frontend of 5 projects by improving the overall UX, simplifying the project structure.",
+    period: "Mar 2015 – Dec 2019",
+    skills: [
+      {
+        name: "HTML",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+      },
+      {
+        name: "CSS",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg",
+      },
+      {
+        name: "Bootstrap",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg",
+      },
+      {
+        name: "Tailwind CSS",
+        link: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+      },
+      {
+        name: "Material UI",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
+      },
+      {
+        name: "Javascript",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+      },
+      {
+        name: "TypeScript",
+        link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+      },
+      {
+        name: "React",
+        link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+      },
+      {
+        name: "Next",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+      },
+      {
+        name: "Redux",
+        link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg",
+      },
+      {
+        name: "React Native",
+        link: "https://pagepro.co/blog/wp-content/uploads/2020/03/react-native-logo-884x1024.png",
+      },
+      {
+        name: "Angular",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg",
+      },
+      {
+        name: "Vue",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg",
+      },
+      {
+        name: "Vuetify",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuetify/vuetify-original.svg",
+      },
+      {
+        name: "jQuery",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original.svg",
+      },
+      {
+        name: "NodeJS",
+        link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+      },
+      {
+        name: "PHP",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg",
+      },
+      {
+        name: "Laravel",
+        link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-plain.svg",
+      },
+    ],
+  },
+];
+
+export const contactLinks = [
+  {
+    name: "Stack Overflow",
+    url: "https://img.icons8.com/external-tal-revivo-color-tal-revivo/40/000000/external-stack-overflow-is-a-question-and-answer-site-for-professional-logo-color-tal-revivo.png",
+    link: "https://stackoverflow.com/users/16787076/andrei-shen",
+  },
+  {
+    name: "Github",
+    url: "https://img.icons8.com/doodle/40/000000/github--v1.png",
+    link: "https://github.com/andreishen",
+  },
+  {
+    name: "Skype",
+    url: "https://img.icons8.com/doodle/40/000000/skype--v2.png",
+    link: "https://join.skype.com/invite/sRRCv0yHBgDC",
+  },
+  {
+    name: "Telegram",
+    url: "https://img.icons8.com/doodle/40/000000/telegram--v2.png",
+    link: "https://t.me/andreishen",
+  },
+  {
+    name: "LinkedIn",
+    url: "https://img.icons8.com/doodle/40/000000/linkedin--v2.png",
+    link: "https://www.linkedin.com/in/andrei-shen/",
+  },
+  {
+    name: "Calendly",
+    url: "https://img.icons8.com/doodle/40/000000/calendar--v2.png",
+    link: "https://calendly.com/andrei-shen-interview/30min",
+  },
+];
+
+export const serviceData = [
+  {
+    name: "Front-end Development",
+    desc: "Front-end development is an important part of web development, as the user interface is often the first thing that users see and interact with when using a website or application. A well-designed and responsive user interface can help to improve user engagement, reduce bounce rates, and increase conversions.",
+    img: "https://img.icons8.com/external-kiranshastry-lineal-color-kiranshastry/64/000000/external-web-development-coding-kiranshastry-lineal-color-kiranshastry.png",
+  },
+  {
+    name: "Mobile Application Development",
+    desc: `Mobile app development is an important part of the digital economy, as more and more users access the internet and services through their mobile devices. A well-designed and functional mobile app can help businesses to reach and engage with their customers, improve user experience, and increase revenue.`,
+    img: "https://img.icons8.com/external-justicon-lineal-color-justicon/64/000000/external-app-development-responsive-web-design-justicon-lineal-color-justicon.png",
+  },
+
+  {
+    name: "Backend Development",
+    desc: `Backend development is an important part of web development, as it provides the foundation for the functionality and performance of web applications. A well-designed and well-architected backend system can help to ensure the security, scalability, and reliability of web apps, as well as improve their performance & speed.`,
+    img: "https://img.icons8.com/external-flaticons-flat-flat-icons/64/000000/external-backend-no-code-flaticons-flat-flat-icons.png",
+  },
+];
